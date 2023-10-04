b02a2013ec430780025e9bd6a3e49e29207f15be
Ankit <ankitbhawsar206@gmail.com>
2023-10-04 17:00:28 +0530

    Fix line endings to LF

--- server/routes/room.js
+++ server/routes/room.js
@@ -1,4 +1,3 @@
-// routes/room.js
 const express = require('express');
 const router = express.Router();
 const Room = require('../models/Room');
@@ -45,7 +44,7 @@ router.get('/download', (req, res) => {
   const fileStream = fs.createReadStream(fileLocation);
 
   fileStream.on('error', () => {
-      res.status(404).send('File not found');
+    res.status(404).send('File not found');
   });
 
   // Determine the content type based on the file extension
@@ -53,9 +52,9 @@ router.get('/download', (req, res) => {
   const fileExtension = path.extname(filePath).toLowerCase();
 
   if (fileExtension === '.pdf') {
-      contentType = 'application/pdf';
+    contentType = 'application/pdf';
   } else if (fileExtension === '.xls' || fileExtension === '.xlsx') {
-      contentType = 'application/vnd.ms-excel';
+    contentType = 'application/vnd.ms-excel';
   }
 
   // Set appropriate response headers
@@ -83,21 +82,21 @@ router.get('/download', (req, res) => {
 
 // Create a room
 router.post('/create', async (req, res) => {
-    console.log("/create");
-    try {
-      const { roomCode } = req.body;
-      const room = new Room({ roomCode });
-      await room.save();
-      res.json(room);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to create a room' });
-    }
-  });
-  
+  console.log("/create");
+  try {
+    const { roomCode } = req.body;
+    const room = new Room({ roomCode });
+    await room.save();
+    res.json(room);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create a room' });
+  }
+});
+
 
 // Get room by code
 router.get('/:code', async (req, res) => {
-    console.log("/code");
+  console.log("/code");
   try {
     const { code } = req.params;
     const room = await Room.findOne({ roomCode: code });
@@ -111,32 +110,32 @@ router.get('/:code', async (req, res) => {
 });
 // Upload a file to the room
 router.post('/:code/upload', upload.single('file'), async (req, res) => {
-    console.log("/code/upload");
-    try {
-      const { code } = req.params;
-      const room = await Room.findOne({ roomCode: code });
-      if (!room) {
-        return res.status(404).json({ error: 'Room not found' });
-      }
-  
-      const { file } = req;
-      const isText = false;
-      const fileName = file.originalname;
-      const filePath = file.path.replace('uploads/', ''); // Remove 'uploads/' from the path
-  
-      room.uploads.push({ isText, fileName, filePath }); // Store the corrected filePath
-      await room.save();
-  
-      res.json({ message: 'File uploaded successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to upload file' });
+  console.log("/code/upload");
+  try {
+    const { code } = req.params;
+    const room = await Room.findOne({ roomCode: code });
+    if (!room) {
+      return res.status(404).json({ error: 'Room not found' });
     }
-  });
-  
+
+    const { file } = req;
+    const isText = false;
+    const fileName = file.originalname;
+    const filePath = file.path.replace('uploads/', ''); // Remove 'uploads/' from the path
+
+    room.uploads.push({ isText, fileName, filePath }); // Store the corrected filePath
+    await room.save();
+
+    res.json({ message: 'File uploaded successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to upload file' });
+  }
+});
+
 
 // Upload text to the room
 router.post('/:code/upload-text', async (req, res) => {
-    console.log("/code/upload text");
+  console.log("/code/upload text");
   try {
     const { code } = req.params;
     const room = await Room.findOne({ roomCode: code });
@@ -158,7 +157,7 @@ router.post('/:code/upload-text', async (req, res) => {
 
 // Get uploads in the room
 router.get('/:code/uploads', async (req, res) => {
-    console.log("code/uploads");
+  console.log("code/uploads");
   try {
     const { code } = req.params;
     const room = await Room.findOne({ roomCode: code });
